8ca1c5d251969322e6aacb29511101426378b672
sashauly <77851963+sashauly@users.noreply.github.com>
2025-05-10 13:06:36 +0300

    move zod schema to a new file

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,42 +1,61 @@
 import { z } from "zod";
 
-export const RecipeSchema = z.object({
-  name: z.string().min(1, "Recipe name is required"),
-  description: z.string().min(1, "Description is required"),
-  category: z.array(z.string()).default([]).optional(),
-  cuisine: z.array(z.string()).default([]).optional(),
+// TODO add russian translations
+export const recipeFormSchema = z.object({
+  name: z.string().refine((value) => value.trim().length >= 1, {
+    message: "Name is required and cannot contain only whitespace",
+  }),
+  prepTime: z.coerce.number().min(1, "Preparation time must be at least 1"),
+  cookTime: z.coerce.number().min(1, "Cooking time must be at least 1"),
+  servings: z.coerce.number().min(1, "Servings must be at least 1"),
+  keywords: z.array(z.string()).default([]).optional(),
   ingredients: z
-    .array(
-      z.object({
-        name: z.string().min(1, "Ingredient name is required"),
-        amount: z.number().min(0, "Amount must be a positive number"),
-        unit: z.string(),
-      })
-    )
-    .refine((ingredients) => ingredients.length > 0, {
-      message: "At least one ingredient is required",
-    }),
+    .array(z.string().min(1, "Ingredient name cannot be empty"))
+    .min(1, "At least one ingredient is required"),
   instructions: z
     .array(z.string().min(1, "Instruction step cannot be empty"))
-    .refine((instructions) => instructions.length > 0, {
-      message: "At least one instruction is required",
-    }),
-  // image: z.object({
-  //   name: z.string(),
-  //   data: z.string(),
-  // }),
-  servings: z.number().min(1, "Servings must be at least 1"),
-  prepTime: z.string().optional(),
-  cookTime: z.string(),
-  totalTime: z.string().optional(),
-  author: z.string().min(1, "Author is required"),
-  keywords: z
-    .array(
-      z.string().refine((value) => value.trim().length >= 1, {
-        message: "tags is required and cannot contain only whitespace",
-      })
-    )
-    .optional(),
+    .min(1, "At least one instruction step is required"),
 });
 
-export type RecipeFormValues = z.infer<typeof RecipeSchema>;
+export type RecipeFormValues = z.infer<typeof recipeFormSchema>;
+
+// const RecipeSchema = z.object({
+//   name: z.string().min(1, "Recipe name is required"),
+//   description: z.string().min(1, "Description is required"),
+//   category: z.array(z.string()).default([]).optional(),
+//   cuisine: z.array(z.string()).default([]).optional(),
+//   ingredients: z
+//     .array(
+//       z.object({
+//         name: z.string().min(1, "Ingredient name is required"),
+//         amount: z.number().min(0, "Amount must be a positive number"),
+//         unit: z.string(),
+//       })
+//     )
+//     .refine((ingredients) => ingredients.length > 0, {
+//       message: "At least one ingredient is required",
+//     }),
+//   instructions: z
+//     .array(z.string().min(1, "Instruction step cannot be empty"))
+//     .refine((instructions) => instructions.length > 0, {
+//       message: "At least one instruction is required",
+//     }),
+//   // image: z.object({
+//   //   name: z.string(),
+//   //   data: z.string(),
+//   // }),
+//   servings: z.number().min(1, "Servings must be at least 1"),
+//   prepTime: z.string().optional(),
+//   cookTime: z.string(),
+//   totalTime: z.string().optional(),
+//   author: z.string().min(1, "Author is required"),
+//   keywords: z
+//     .array(
+//       z.string().refine((value) => value.trim().length >= 1, {
+//         message: "tags is required and cannot contain only whitespace",
+//       })
+//     )
+//     .optional(),
+// });
+
+// type RecipeFormValues = z.infer<typeof RecipeSchema>;
