1a55e188bd3ce72918d6837c90ab7953bde9e903
sashauly <77851963+sashauly@users.noreply.github.com>
2025-05-18 18:04:04 +0300

    add reload prompt component

---
+++ src/components/ReloadPrompt.tsx
@@ -0,0 +1,69 @@
+import { useRegisterSW } from "virtual:pwa-register/react";
+import { Button } from "@/components/ui/button";
+import { logDebug } from "@/lib/utils/logger";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useTranslation } from "react-i18next";
+
+function ReloadPrompt() {
+  const { t } = useTranslation();
+  const {
+    offlineReady: [offlineReady, setOfflineReady],
+    needRefresh: [needRefresh, setNeedRefresh],
+    updateServiceWorker,
+  } = useRegisterSW({
+    onRegistered(r) {
+      logDebug("SW Registered: " + r);
+    },
+    onRegisterError(error) {
+      logDebug("SW registration error", error);
+    },
+  });
+
+  const close = () => {
+    setOfflineReady(false);
+    setNeedRefresh(false);
+  };
+
+  return (
+    <div>
+      {(offlineReady || needRefresh) && (
+        <Card className="fixed right-0 bottom-0 p-3 m-4 max-w-sm z-50">
+          <CardHeader className="p-0 mb-2">
+            <CardTitle className="text-lg">
+              {offlineReady ? (
+                <h2>{t("reloadPrompt.readyForOfflineUse")}</h2>
+              ) : (
+                <h2>{t("reloadPrompt.newUpdateAvailable")}</h2>
+              )}
+            </CardTitle>
+          </CardHeader>
+          <CardContent className="p-0 mb-4 text-sm text-muted-foreground">
+            {offlineReady ? (
+              <p>{t("reloadPrompt.offlineDescription")}</p>
+            ) : (
+              <p>{t("reloadPrompt.updateDescription")}</p>
+            )}
+          </CardContent>
+          <CardFooter className="flex justify-end gap-2 p-0">
+            <Button variant="outline" onClick={() => close()}>
+              {t("common.close")}
+            </Button>
+            {needRefresh && (
+              <Button onClick={() => updateServiceWorker(true)}>
+                {t("common.reload")}
+              </Button>
+            )}
+          </CardFooter>
+        </Card>
+      )}
+    </div>
+  );
+}
+
+export default ReloadPrompt;
