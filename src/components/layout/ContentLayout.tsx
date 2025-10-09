9da5a223c4ac400c7638f351b15b7020cebd484e
sashauly <77851963+sashauly@users.noreply.github.com>
2025-10-09 12:52:02 +0300

    fix styles for contentLayout

--- src/components/layout/ContentLayout.tsx
+++ src/components/layout/ContentLayout.tsx
@@ -17,8 +17,8 @@ export function ContentLayout({
       <Header title={title} />
       <div
         className={cn(
-          "flex justify-center flex-1 py-2 sm:py-4 px-4 sm:px-6 ",
-          className,
+          "flex justify-center flex-1 py-2 md:py-4 px-4 sm:px-6",
+          className
         )}
       >
         {children}
